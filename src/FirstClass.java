ce0fbb174aaade668603a1df5400f4c9897d863c
1Servant96 <zhumataev.03@gmail.com>
2022-10-06 12:23:02 +0600

    Hello

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,4 +1,9 @@
 public class FirstClass {
     private String name;
     private String surName;
+
+    public FirstClass(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
 }
