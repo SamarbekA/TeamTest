1a948d5dae6002d8b0326088a55f4975ba69bfe8
osorov97 <osorov.nursultan97@gmail.com>
2022-10-06 12:35:22 +0600

    setter and getter

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -6,4 +6,20 @@ public class FirstClass {
         this.name = name;
         this.surName = surName;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }
